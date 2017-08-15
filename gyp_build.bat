957d1094158aa7fb3cafb01b737b2df368f2871d
boolking <boolking@gmail.com>
2017-08-15 08:52:20 +0800

    1. Fix snprintf() conflict for VC. 2. Fix macro redefinition warnings for va_copy.

--- gyp_build.bat
+++ gyp_build.bat
@@ -1,3 +1,4 @@
 copy /y third_party\leveldb_win\port\port.h third_party\leveldb\port
+copy /y third_party\glog_win\* third_party\glog\src\windows
 python third_party\gyp\gyp_main.py --depth=. --include=common.gypi -Dtarget_arch=ia32 --generator-output=. -G msv_version=2017 -f msvs phxpaxos.gyp
 
