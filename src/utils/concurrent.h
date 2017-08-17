59733b26637fb5e7e28407502df59658eca807f7
boolking <boolking@gmail.com>
2017-08-17 09:10:38 +0800

    Rewrite Thread with std::thread and remove unportable ThreadAttr, Thread::start(ThreadAttr&) and Thread::kill(int).

--- src/utils/concurrent.h
+++ src/utils/concurrent.h
@@ -37,31 +37,12 @@ See the AUTHORS file for names of contributors.
 #include <string>
 #include <condition_variable>
 #include <mutex>
+#include <thread>
 
 namespace phxpaxos {
 
 using std::deque;
 
-class ThreadAttr {
-public:
-    ThreadAttr();
-
-    ~ThreadAttr();
-
-    void setScope(bool sys);
-
-    void setStackSize(size_t n);
-
-    void setDetached(bool detached);
-
-    void setPriority(int prio);
-    
-    pthread_attr_t* impl();
-
-private:
-
-    pthread_attr_t _attr;
-};
 
 class Thread : public Noncopyable {
 public:
@@ -71,22 +52,18 @@ public:
 
     void start();
 
-    void start(ThreadAttr& attr);
-
     void join();
 
     void detach();
     
-    pthread_t getId() const;
-
-    void kill(int sig);
+    std::thread::id getId() const;
 
     virtual void run() = 0;
 
     static void sleep(int ms);
 
 protected:
-    pthread_t _thread;
+    std::thread _thread;
 };
 
 template <class T>
