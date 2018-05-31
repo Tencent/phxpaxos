a88e21d6fad5eab65d2da9c429556d0c51deeae4
taohe <102320170@qq.com>
2018-05-31 20:07:04 +0800

    fix third party

--- build.sh
+++ build.sh
@@ -2,43 +2,9 @@
 set -e  # exit immediately on error
 set -x  # display all commands
 
-cd third_party;
+git submodule update --init --recursive
 
-if [ ! -f protobuf/bin/protoc ]; then
-	if [ ! -f protobuf-cpp-3.0.0.tar.gz ]; then
-		wget https://github.com/google/protobuf/releases/download/v3.0.0/protobuf-cpp-3.0.0.tar.gz
-	fi	
-
-	tar zxvf protobuf-cpp-3.0.0.tar.gz
-	cd protobuf-3.0.0
-
-	./configure --prefix=`pwd`/../protobuf
-	make -j2
-	make install
-
-	cd ../
-fi
-
-if [ ! -f leveldb/lib/libleveldb.a ]; then
-	if [ ! -f v1.19.tar.gz ]; then
-		wget https://github.com/google/leveldb/archive/v1.19.tar.gz
-	fi
-
-	rm -rf leveldb
-
-	tar zxvf v1.19.tar.gz
-	ln -s leveldb-1.19 leveldb
-	cd leveldb
-
-	make
-
-	mkdir -p lib
-	cp out-static/libleveldb.a lib/
-
-	cd ../
-fi
-
-cd ..
+(cd third_party && bash ./autoinstall.sh)
 
 ./autoinstall.sh
 
