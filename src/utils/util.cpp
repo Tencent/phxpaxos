dd8d57c70c5fbb5630ef201d8f336081b956c854
hmjmf <31787550+hmjmf@users.noreply.github.com>
2023-12-04 15:52:18 +0800

    fix arm cpu

--- src/utils/util.cpp
+++ src/utils/util.cpp
@@ -229,6 +229,25 @@ __inline__ uint64_t rdtsc()
     return (d<<32) | a;
 }
 
+#elif __aarch64__
+
+__inline__ uint64_t rdtsc()
+{
+
+    uint64_t val;
+
+    /*
+     * According to ARM DDI 0487F.c, from Armv8.0 to Armv8.5 inclusive, the
+     * system counter is at least 56 bits wide; from Armv8.6, the counter
+     * must be 64 bits wide.  So the system counter could be less than 64
+     * bits wide and it is attributed with the flag 'cap_user_time_short'
+     * is true.
+     */
+    asm volatile("mrs %0, cntvct_el0" : "=r" (val));
+
+    return val;
+}
+
 #endif
 
 struct FastRandomSeed {
